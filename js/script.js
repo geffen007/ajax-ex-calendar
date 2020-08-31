da049ed33438baae1ab1eca64aec2cd51accc19d
Guido <guido.amodio@gmail.com>
2020-08-31 19:18:59 +0200

    giusto un pò di stile

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-// Creiamo un calendario dinamico con le festività.
-// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
-// Milestone 1
-// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
-// Milestone 2
-// Diamo la possibilità di cambiare mese, gestendo il caso in cui l’API non possa ritornare festività.
-// Attenzione!
-// Ogni volta che cambio mese dovrò:
-// Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018)
-// Controllare quanti giorni ha il mese scelto formando così una lista
-// Chiedere all’API quali sono le festività per il mese scelto
-
-
-
-//https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
-// "response": [
-//     {
-//         "name": "Capodanno",
-//         "date": "2018-01-01"
-//     }
-
-
-
-
 $(document).ready(function() {
 
     var attributo = $('h1').attr('data-this-date');
@@ -109,7 +85,7 @@ function insertHolidays(data){
                 var arr = resp.response;
                 for (var i = 0; i < arr.length; i++) {
                     var listItem = $('li[data-complete-date = "' + arr[i].date + '"]');
-                    listItem.append('-' + arr[i].name);
+                    listItem.append(' - ' + arr[i].name);
                     listItem.addClass('holiday');
                 }
 
